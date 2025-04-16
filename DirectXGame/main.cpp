f038cad517d92c09c80a041ec785df5fbd9623aa
onknjpeel <k023g0103@g.neec.ac.jp>
2025-04-16 15:01:21 +0900

    01_02基礎プログラム

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,3 +1,4 @@
+#include "GameScene.h"
 #include "KamataEngine.h"
 #include <Windows.h>
 #include <d3dcompiler.h>
@@ -6,83 +7,42 @@ using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	
-	//エンジンの初期化
+
+	// エンジンの初期化
 	KamataEngine::Initialize(L"LE3C_07_オノ_ケンジピエール");
 
-	//DirectXCommonのインスタンスの取得
+	// DirectXCommonのインスタンスの取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
-	// DirectXCommonクラスが管理している、ウィンドウの幅と高さの取得
-	int32_t w = dxCommon->GetBackBufferWidth();
-	int32_t h = dxCommon->GetBackBufferHeight();
-	DebugText::GetInstance()->ConsolePrintf(std::format("width: {}, height: {}\n", w, h).c_str());
+	// ゲームシーンのインスタンス生成
+	GameScene* gameScene = new GameScene();
+	// ゲームシーンの初期化
+	gameScene->Initialize();
 
-	// DirectXCommonクラスが管理している、コマンドリストの取得
-	ID3D12GraphicsCommandList* commandList = dxCommon->GetCommandList();
-
-	// RootSignature作成 -----------------------------------
-	// 構造体にデータを用意する
-	D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
-	descriptionRootSignature.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
-	ID3DBlob* signatureBlob = nullptr;
-	ID3DBlob* errorBlog = nullptr;
-	HRESULT hr = D3D12SerializeRootSignature(&descriptionRootSignature, D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob, &errorBlog);
-	if (FAILED(hr)) {
-		DebugText::GetInstance()->ConsolePrintf(reinterpret_cast<char*>(errorBlog->GetBufferPointer()));
-		assert(false);
-	}
-
-	// バイナリをもとに生成
-	ID3D12RootSignature* rootSignature = nullptr;
-	hr = dxCommon->GetDevice()->CreateRootSignature(0, signatureBlob->GetBufferPointer(), signatureBlob->GetBufferSize(), IID_PPV_ARGS(&rootSignature));
-	assert(SUCCEEDED(hr));
-
-	// InputLayOut
-	D3D12_INPUT_ELEMENT_DESC inputElementDescs[1] = {};
-	inputElementDescs[0].SemanticName = "POSITION";
-	inputElementDescs[0].SemanticIndex = 0;
-	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
-	inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
-	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
-	inputLayoutDesc.pInputElementDescs = inputElementDescs;
-	inputLayoutDesc.NumElements = _countof(inputElementDescs);
-
-	// BlendState ------------------  今回は不透明
-	D3D12_BLEND_DESC blendDesc{};
-	// 全ての色要素を書き込む
-	blendDesc.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
-
-	// RasterizerState ---------------
-	D3D12_RASTERIZER_DESC rasterizerDesc{};
-	// 裏面(反時計回り)をカリングする
-	rasterizerDesc.CullMode = D3D12_CULL_MODE_BACK;
-	// 塗りつぶしモードをソリッドにする(ワイヤーフレームならD3D12_FILL_MODE_WIREFRAME)
-	rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
-
-	// コンパイル済みのShader、エラー時情報の格納場所の用意
-	ID3DBlob* vsBlob = nullptr;
-	ID3DBlob* psBloob = nullptr;
-	ID3DBlob* errorBlob = nullptr;
-	// 頂点シェーダの読み込みとコンパイル
-	std::wstring vsFile = L"Resources/shaders/TestVS.hlsl";
-	hr = D3DCompileFromFile(vsFile.c_str(), nullptr, D3D_COMPILE_STANDARD_FILE_INCLUDE, "main", "vs_5_0", D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, 0, &vsBlob, &errorBlob);
-
-	//メインループ
+	// メインループ
 	while (true) {
 		if (KamataEngine::Update()) {
 			break;
 		}
 
-	//描画開始
-	dxCommon->PreDraw();
+		// ゲームシーンの更新
+		gameScene->Update();
 
-	//描画終了
-	dxCommon->PostDraw();
+		// 描画開始
+		dxCommon->PreDraw();
 
+		//ゲームシーンの描画
+		gameScene->Draw();
+
+		// 描画終了
+		dxCommon->PostDraw();
 	}
 
-	//エンジンの終了処理
+	delete gameScene;
+
+	gameScene = nullptr;
+
+	// エンジンの終了処理
 	KamataEngine::Finalize();
 
 	return 0;
