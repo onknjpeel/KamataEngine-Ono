7252784a0ca321658e9be75d642cdd8d3ca022d9
onknjpeel <k023g0103@g.neec.ac.jp>
2025-05-15 10:53:10 +0900

    課題

--- DirectXGame/Effect.cpp
+++ DirectXGame/Effect.cpp
@@ -29,7 +29,9 @@ void Effect::Update() {
 		isFinished_ = true;
 	}
 
-	worldTransform_.scale_.x = std::clamp(1.0f - counter_ / kDuration, 0.0f, 0.5f);
+	worldTransform_.scale_.x = std::clamp(1.0f - counter_ / kDuration, 0.0f, 0.25f);
+
+	worldTransform_.rotation_.z += std::clamp(1.0f - counter_ / kDuration, 0.0f, 0.25f);
 
 	worldTransform_.UpdateMatrix();
 
